df291ca59e4cd349c8791acf36773675fc813932
Renée Kooi <renee@kooi.me>
2018-04-07 20:47:56 +0200

    Give it a name.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import path from 'path';
 import emoji from 'emoji.json';
 import serveStatic from 'serve-static';
 
+const name = 'EmojiOne';
+
 function middleware() {
   const baseDir = path.join(__dirname, './emoji');
   return serveStatic(baseDir);
 }
 
 export {
+  name,
   emoji,
   middleware,
 };
